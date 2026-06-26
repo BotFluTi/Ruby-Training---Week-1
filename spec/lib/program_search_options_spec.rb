2c5f7119fd9d9ee4a7e40da3d7d520f4f9d84d8c
BotFluTi <vladardelean000@gmail.com>
2026-06-26 19:20:59 +0300

    ordered by downloads test

---
+++ spec/lib/program_search_options_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require './lib/program'
+require './lib/gem_data'
+require './lib/ruby_gems_api_client'
+
+RSpec.describe 'Program search options' do
+  describe '#execute' do
+    subject(:execute) { Program.new(client).execute(args) }
+
+    let(:client) { instance_double(RubyGemsApiClient) }
+
+    context 'when search has --most-downloads-first option' do
+      let(:args) { %w[search rails --most-downloads-first] }
+
+      before do
+        allow(client).to receive(:search) do
+          [
+            GemData.new('small-gem', 'Few downloads', 10),
+            GemData.new('popular-gem', 'Many downloads', 100)
+          ]
+        end
+      end
+
+      it 'returns gems ordered by downloads descending' do
+        expect(execute.output).to eq(
+          "popular-gem - Many downloads\nsmall-gem - Few downloads"
+        )
+      end
+    end
+  end
+end
